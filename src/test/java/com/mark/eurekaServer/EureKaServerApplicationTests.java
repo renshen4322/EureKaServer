7ca8c74a75f8191d59dcb241bc87f6a067160799
hudonghui <234498142@qq.com>
2022-04-27 13:18:32 +0800

    init 0

---
+++ src/test/java/com/mark/eurekaServer/EureKaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EureKaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
